88f727409ce992dca865d83fbad330abe74d891e
FENCSUI <116577106+FENCSUI@users.noreply.github.com>
2023-01-25 22:06:33 +0300

    api generate random fact

---
+++ javascript.js
@@ -0,0 +1,8 @@
+async function randomFact(){
+    const res = await fetch('https://api.chucknorris.io/jokes/random')
+    const data = await res.json();
+    const fact = data.value;
+    const norisfact=document.getElementById("chuckfact");
+    norisfact.innerText=fact
+  }
+  window.onload = randomFact();
