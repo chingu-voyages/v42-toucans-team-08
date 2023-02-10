0a6bf9426b420bd60432f945aa6331117de0593e
ofthewildfire <lissajace@gmail.com>
2023-02-10 20:30:31 +0200

    Removed comments from JS

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,19 +1,14 @@
 import toggle from "./darkmode.js";
 
-// Taken it out of the function scope to be able to reuse it for the user search and not have to declare a new one! - Kirsten
+const form = document.getElementById("form");
 const norisfact = document.getElementById("fact-text");
-// Search bar element
 const searchBar = document.getElementById("search");
-// Search button element
 const searchBtn = document.getElementById("search-btn");
-// The dropdown menu
-const categoryDropdown = document.querySelector("#categories"); //
-// The value collect from the searchBar when user enters it using the eventListener below
+const categoryDropdown = document.querySelector("#categories");
+// Typed value from the Search Bar
 let searchQuery = searchBar.value;
 // Chosen category for API
 let category;
-//Form
-const form = document.getElementById("form");
 
 async function randomFact() {
 	const response = await fetch("https://api.chucknorris.io/jokes/random");
@@ -30,7 +25,6 @@ window.onload = randomFact();
 document.getElementById("random-fact").addEventListener("click", randomFact);
 
 //Search bar - string API function
-
 async function searchJoke() {
 	const res = await fetch(
 		`https://api.chucknorris.io/jokes/search?query=${searchQuery}`
@@ -41,7 +35,6 @@ async function searchJoke() {
 
 	if (dataLength) {
 		const factMap = data.result.map((fact) => {
-			// console.log(fact);
 			return fact.value;
 		});
 		const index = Math.floor(Math.random() * factMap.length);
